e3b415c96e945aa572f9da82af545d3c6028ba05
ShaneMarusczak <marusczaks@gmail.com>
2018-10-21 15:10:05 -0400

    added in content

--- src/main/java/reviewssitefullstack/ReviewSitePopulator.java
+++ src/main/java/reviewssitefullstack/ReviewSitePopulator.java
@@ -16,18 +16,18 @@ public class ReviewSitePopulator implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Category test1 = new Category("category1");
+		Category test1 = new Category("Dogs");
 		test1 = categoryRepo.save(test1);
-		Category test2 = new Category("category2");
+		Category test2 = new Category("Cats");
 		test2 = categoryRepo.save(test2);
-		Category test3 = new Category("category3");
+		Category test3 = new Category("Birds");
 		test3 = categoryRepo.save(test3);
 		
-		Review testReview1 = new Review("review1", "review1", "review1", test1);
+		Review testReview1 = new Review("Spot", "spot.png", "Spot is a carefree puppy that likes to play!", test1);
 		testReview1 = reviewRepo.save(testReview1);
-		Review testReview2 = new Review("review2", "review2", "review2", test2);
+		Review testReview2 = new Review("Garfield", "garfield.jpg", "Garfield is a very lazy and slow cat that loves lasagna.", test2);
 		testReview2 = reviewRepo.save(testReview2);
-		Review testReview3 = new Review("review3", "review3", "review3", test3);
+		Review testReview3 = new Review("Tweety", "tweety.jpg", "Tweety is a smart bird that is always staying just out of reach of Sylvester.", test3);
 		testReview3 = reviewRepo.save(testReview3);
 	}
 	
